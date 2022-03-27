af317c03e633ca5009acc0585379cf974843e4b9
Gourab-18 <gourabomm2001@gmail.com>
2022-03-27 14:20:44 +0530

    minor change

--- client/src/App.js
+++ client/src/App.js
@@ -85,11 +85,8 @@ function App() {
         <Route path="/creator/team" element={<ManageTeam />} />
         <Route path="/Upload" element={<UploadVideo />} />
         <Route path="/Creator" element={<Sidebar />} />
-<<<<<<< HEAD
         <Route path="/profile" element={<Profile />} />
-=======
         <Route path="/Signup" element={<Signup />} />
->>>>>>> 5ac0cba9244e071f74d61ee7295b50b8fb4b9087
       </Routes>
     </Router>
   );
